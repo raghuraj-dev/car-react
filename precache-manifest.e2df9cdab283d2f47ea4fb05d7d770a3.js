4ce1f9b575990f507afd3fdbc3a12aa804792b7b
raghuraj20 <balasubramaniam.raghuraj@gmail.com>
2020-09-11 22:56:12 +0530

    Updates

---
+++ precache-manifest.e2df9cdab283d2f47ea4fb05d7d770a3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc800884adbba2e97a7ff0c73d4aeb5c",
+    "url": "/car-react/index.html"
+  },
+  {
+    "revision": "3fb1cff1eeb461e322cd",
+    "url": "/car-react/static/js/2.3cb3d2ca.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/car-react/static/js/2.3cb3d2ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81250998266eae215bfd",
+    "url": "/car-react/static/js/main.f6bdc4aa.chunk.js"
+  },
+  {
+    "revision": "b8d597b99ac0dad183a5",
+    "url": "/car-react/static/js/runtime-main.11308365.js"
+  }
+]);
